169e7f9694677ceeb2c934f3c994a10641690949
Fariz Anjum <farizanjum2018@gmail.com>
2024-12-10 20:25:21 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,4 @@
-from app import create_app
-import os
-
-app = create_app()
+from app import app
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
