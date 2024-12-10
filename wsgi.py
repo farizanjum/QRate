392d73c1222da156b570e614a2a8c56f12868fcb
Fariz Anjum <farizanjum2018@gmail.com>
2024-12-10 18:51:50 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,8 @@
 from app import create_app
+import os
 
 app = create_app()
+
+if __name__ == "__main__":
+    # This part is for local development only
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
