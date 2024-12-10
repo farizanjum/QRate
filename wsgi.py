e300a63f418d655ab6475ae2cf52b9506fd5b115
Fariz Anjum <farizanjum2018@gmail.com>
2024-12-10 20:48:45 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
-from app import app
+import os
+from app import create_app
+
+app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # Use 5000 as the default port
+    app.run(host="0.0.0.0", port=port)
